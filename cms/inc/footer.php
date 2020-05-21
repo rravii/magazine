bbb3361fa557dd7323acaa6b6e554f67f3b6ec8b
rravii <panditravi217@gmail.com>
2020-05-21 13:53:25 +0545

    Day 1 project completed

---
+++ cms/inc/footer.php
@@ -0,0 +1,24 @@
+       <!-- footer content -->
+       <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="js/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="nprogress/nprogress.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="js/custom.min.js"></script>
+  </body>
+</html>
